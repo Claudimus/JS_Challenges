c500ca39a97abe5924d80ddcc5d2370f34d8ea5c
Claudio <119994996+Claudimus@users.noreply.github.com>
2023-07-28 16:46:18 -0400

    Add files via upload

---
+++ public-space-flights-javascript/main.js
@@ -0,0 +1,40 @@
+// Your code here!
+//************************************************ 
+//Part 1 
+//******* 
+//Greet Customers 
+alert(`Hello customer ! Welcome to out exclusive space flight tours.`); 
+//Declare adultTickets as a variable 
+// prompt customers and Ask customers how many adult Tickets they want. Store it as adultTickets variable
+//Accept the user's answer and save it as a variable
+let adultTickets = prompt(`How many adult tickets would you like ?`);
+//Declare childTickets variable
+//prompt customers by asking how many child tickets do they want
+//Accept the user input and save it as a variable 
+let childTickets= prompt(`How many child tickets do you want?`);
+//Print a returning number of adult and child ticket the user asked for 
+alert(`You have ordered ${adultTickets} adult tickets and ${childTickets} child tickets`);
+console.log(`You have ordered ${adultTickets} adult tickets and ${childTickets} child tickets`);
+
+
+
+//**************************************************** 
+// PART 2
+//****************************************************
+
+const dehydratedBanana = 1.27
+const tofuCake = 4.17
+const cheeseSpread = 3.79
+const dehydratedIceCreamSandwich = 0.75
+//console.log(`It's working so far!`)
+
+alert (`Two tofu cakes and two dehydrated ice cream sandwiches cost $${2*dehydratedIceCreamSandwich + 2*tofuCake}.`);
+alert (`Eight cheese spreads and a dehydrated banana cost $${8*cheeseSpread + dehydratedBanana}.`);
+alert(`Three of each item on the menu cost $${3*dehydratedIceCreamSandwich + 3*tofuCake + 3*cheeseSpread + 3*dehydratedBanana}.`);
+
+
+
+
+
+
+
