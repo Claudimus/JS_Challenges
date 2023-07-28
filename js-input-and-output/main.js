3160fc78881ecb75498f834486765f2d954e81a2
Claudio <119994996+Claudimus@users.noreply.github.com>
2023-07-28 16:44:02 -0400

    Add files via upload

---
+++ js-input-and-output/main.js
@@ -0,0 +1,12 @@
+let flightLeader
+flightLeader = prompt("Who is this excursion's flight leader?")
+confirm("Are you sure this is the correct flight leader?")
+// console.log prints the data
+const flightDistance = parseInt(prompt("How many miles is this flight?"))
+// console.log prints the data
+console.log(flightDistance)
+// typeof prints the type of data
+typeof flightDistance
+alert(`${flightLeader} is leading this excursion.`)
+console.log(`Today, your flight leader is ${flightLeader}`)
+
